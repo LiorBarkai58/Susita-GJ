502a7e1f4964701118c3937043618f27145bd1b4
MRDEATHCURE <ido.drutin@gmail.com>
2024-07-05 15:49:45 +0300

    Create new roads on collision

--- Assets/Scripts/FloorGeneration.cs
+++ Assets/Scripts/FloorGeneration.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FloorGeneration : MonoBehaviour
 {
     [SerializeField] List<GameObject> floors;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,13 @@ public class FloorGeneration : MonoBehaviour
         
     }
     void OnTriggerEnter(Collider collider){
-        Debug.Log("test");
-        Instantiate(floors[Random.Range(0, floors.Count)], new Vector3(25, 0,0), Quaternion.identity);
+        Debug.Log(collider.GetComponentInParent<Transform>().position);
+        Vector3 roadPos = collider.transform.parent.position + new Vector3(-50, 0, 0);
+        GameObject floor = ObjectPool.SharedInstance.GetPooledObject();
+        if (floor != null)
+        {
+            floor.transform.position = roadPos;
+            floor.SetActive(true);
+        }
     }
 }
