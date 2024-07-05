2e15bcec70a614d1cef4f0fb6e6a45ada09078b1
Lior Barkai <lior5858@gmail.com>
2024-07-06 00:51:21 +0300

    Organize floor generation

--- Assets/Scripts/FloorGeneration.cs
+++ Assets/Scripts/FloorGeneration.cs
@@ -10,7 +10,12 @@ public class FloorGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject floor = ObjectPool.SharedInstance.GetPooledObject();
+            if (floor != null)
+            {
+                floor.transform.position = new Vector3(0,0,0);
+                floor.SetActive(true);
+            }
     }
 
     // Update is called once per frame
