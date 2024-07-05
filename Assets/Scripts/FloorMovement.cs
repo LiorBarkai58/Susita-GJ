586960178a42d2cf1b42e8605a0120fc1f73db15
MRDEATHCURE <ido.drutin@gmail.com>
2024-07-05 15:48:36 +0300

    Increase floor speed

--- Assets/Scripts/FloorMovement.cs
+++ Assets/Scripts/FloorMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FloorMovement : MonoBehaviour
 {
-    [SerializeField] float floorSpeed = 2;
+    private float floorSpeed = 7;
     
     // Start is called before the first frame update
     void Start()
