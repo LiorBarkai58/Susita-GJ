b8979e7dd5fa3b93052d48d51d9c139823513d19
Lior Barkai <lior5858@gmail.com>
2024-07-08 18:25:51 +0300

    Create general cooldown class

---
+++ Assets/Scripts/Utility/Cooldown.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown   
+{
+    private float duration = 0f;//Maximum duration of a timer
+    private float completeTime = 0f;
+
+
+
+    public Cooldown(float duration){
+        this.duration = duration;
+        completeTime = 0;
+    }
+    
+    public void startCooldown(){
+        completeTime = Time.time + duration;
+    }
+    public bool IsReady(){
+        Debug.Log($"{Time.time} + {completeTime}");
+        return Time.time > completeTime;
+    }
+}
