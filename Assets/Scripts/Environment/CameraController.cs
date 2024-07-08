1a12c9c8e3a2b4cfbffc393e268b28e984a819dd
Lior Barkai <lior5858@gmail.com>
2024-07-08 18:26:41 +0300

    Adjuts camera to follow player

--- Assets/Scripts/Environment/CameraController.cs
+++ Assets/Scripts/Environment/CameraController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private float position = 0;
-    private float cameraSpeed = 8;
+    [SerializeField] private GameObject player;
+
+    private float zRatio = 0.65f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +18,8 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, position*3), Time.deltaTime*cameraSpeed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z*zRatio);
     }
 
-    public void moveLeft(){
-        position = Math.Clamp(position - 1, -1, 0);
-
-    }
-    public void moveRight(){
-        position = Math.Clamp(position + 1, 0, 1);
-    }
+    
 }
