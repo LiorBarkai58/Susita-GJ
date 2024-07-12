aaed4435ff32dd596928fb49d7a6883178e3e966
Cornetto <lior5858@gmail.com>
2024-07-12 22:31:25 +0300

    Add comment to explain behavior of script

--- Assets/Scripts/Characters/Player/Collecting.cs
+++ Assets/Scripts/Characters/Player/Collecting.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Collecting : MonoBehaviour
 {
+    //Will destroy and object with the tag "collectable"
+    //Currency logic is yet to be implemented
     void OnTriggerEnter(Collider collider){
         if(collider.gameObject.tag == "Collectable"){
             Debug.Log("Collected");
