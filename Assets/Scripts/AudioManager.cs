e3cfd8b46cc16527f9ccc4a93b32ff648f2c99a9
Madar215 <galmadar2424@gmail.com>
2024-09-05 13:51:20 +0300

    UI and Audio
    Audio Manager Prefab
    Audio Settings Script
    Pause Script
    Main Menu UI
    Gameplay UI

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip backgroundMusic;
+    [SerializeField] private AudioClip sfx1;
+    [SerializeField] private AudioClip sfx2;
+    [SerializeField] private AudioClip sfx3;
+    [SerializeField] private AudioClip sfx4;
+
+    private void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+    }
+
+    public void PlaySfx(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
