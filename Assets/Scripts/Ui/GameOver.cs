4b0cc47bbbe2e087b4a976f5b3519cc17c6a0e70
Cornetto <lior5858@gmail.com>
2024-09-14 20:46:04 +0300

    Fix haifa port

--- Assets/Scripts/Ui/GameOver.cs
+++ Assets/Scripts/Ui/GameOver.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
+
+    [SerializeField] private Pause pause;
     
     public void Setup(float meters){
         gameObject.SetActive(true);
+        pause.PauseGame();
     }
 }
